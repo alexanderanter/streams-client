f8fddf08d95de1ac60017414c9c99e70c0fbabc7
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-08 00:41:47 +0000

    components skeleton wired up with routes

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamList from '../components/streams/StreamList';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamShow from '../components/streams/StreamShow';
+
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/new" component={StreamCreate} />
+					<Route path="/streams/edit" component={StreamEdit} />
+					<Route path="/streams/delete" component={StreamDelete} />
+					<Route path="/streams/show" component={StreamShow} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
