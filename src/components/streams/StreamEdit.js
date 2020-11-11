05ac6d4556a93ee2a908c9b73cfbc03ca420f71d
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-11 01:35:44 +0000

    getting data from the redux state store to comp

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamEdit = (props) => {
-	console.log(props.match.params.id);
-	return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
+	render() {
+		return (
+			<div>
+				<h2>Edit Stream</h2>
+				<div>{this.props.stream.title}</div>
+				<div>{this.props.stream.description}</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		streams: state.streams,
+		stream: state.streams[ownProps.match.params.id],
+		//selecting the correct stream by getting the selected id from the url,
+		// the id parameter value stored in the match prop
+	};
 };
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStreams })(StreamEdit);
