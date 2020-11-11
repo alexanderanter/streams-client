b891d0c35828a05566deb6234e608ef45ac8f787
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-11 20:52:29 +0000

    refactor StreamDelete into class component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -4,35 +4,40 @@ import history from '../../history';
 import { deleteStream } from '../../actions';
 import { connect } from 'react-redux';
 
-const StreamDelete = (props) => {
-	const onDismiss = () => {
+class StreamDelete extends React.Component {
+	onDismiss = () => {
 		history.push('/');
 	};
-	const onDeleteClick = () => {
-		props.deleteStream(props.match.params.id);
+	onDeleteClick = () => {
+		this.props.deleteStream(this.props.match.params.id);
 	};
-	const action = (
-		<>
-			<button onClick={onDeleteClick} className="ui button primary">
-				Delete
-			</button>
-			<button onClick={() => history.push('/')} className="ui button">
-				Cancel
-			</button>
-		</>
-	);
-	return (
-		<div>
-			StreamDelete
-			<Modal
-				title="Delete Stream"
-				content="Are you sure you want to delete this stream?"
-				actions={action}
-				onDismiss={onDismiss}
-			/>
-		</div>
-	);
-};
+	action() {
+		return (
+			<>
+				<button onClick={this.onDeleteClick} className="ui button primary">
+					Delete
+				</button>
+				<button onClick={() => history.push('/')} className="ui button">
+					Cancel
+				</button>
+			</>
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				StreamDelete
+				<Modal
+					title="Delete Stream"
+					content="Are you sure you want to delete this stream?"
+					actions={this.action()}
+					onDismiss={this.onDismiss}
+				/>
+			</div>
+		);
+	}
+}
 // const mapStateToProps = (state, ownProps) => {
 // 	return {
 // 		stream: state.streams[ownProps.match.params.id],
